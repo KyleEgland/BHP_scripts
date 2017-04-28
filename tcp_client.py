3fa85fa4bfe7277867c9056f88e5c23218b5bdaf
MediocreMarine <kyle.egland@outlook.com>
2017-04-28 08:25:10 -0400

    Add files via upload
    Initial commit

---
+++ tcp_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# TCP Client, Chapter 2, "Black Hat Python"
+# Simple TCP client
+import socket
+
+target_host = "127.0.0.1"
+target_port = 9999
+
+# Create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the client
+client.connect((target_host, target_port))
+
+# send some data
+client.send("ABCDEF")
+
+# Receive some data
+response = client.recv(4096)
+
+print response
